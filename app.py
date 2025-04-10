374a7768a8095c2bc7a0ef8d0e93a37435a2f95d
shaivalya <shaivalya11@gmail.com>
2025-04-11 00:25:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,9 @@ def donate():
 
     return render_template('donate.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
+
 
 
 
